09f036a6eece16d9db99c5a1aecc94b322546d3f
JustinMSU <92278113+JustinMSU@users.noreply.github.com>
2021-12-15 19:58:17 -0500

    Change to file
    Updated the model used

--- retrieval_model_response.py
+++ retrieval_model_response.py
@@ -39,33 +39,6 @@ def load_id(sentence, word_to_id):
 
     return sentence_ids
 
-def load_ids(pair, word_to_id):
-    context_ids = []
-    response_ids = []
-
-    context_cell = pair[0]
-    response_cell = pair[1]
-
-    max_context_len = 160
-    
-    context_words = context_cell.split()
-    if len(context_words) > max_context_len:
-        context_words = context_words[:max_context_len]
-    for word in context_words:
-        if word in word_to_id:
-            context_ids.append(word_to_id[word])
-        else: 
-            context_ids.append(0) #UNK
-    
-    response_words = response_cell.split()
-    for word in response_words:
-        if word in word_to_id:
-            response_ids.append(word_to_id[word])
-        else: 
-            response_ids.append(0)
-
-    return context_ids, response_ids
-
 class Voc:
     def __init__(self):
         self.vocab = {}
@@ -76,7 +49,7 @@ class Voc:
     def save(self):
         torch.save({
                 'voc_dict': self.__dict__,
-            }, os.path.join('Tutorials/dataset/models', 'save_voc.tar'))
+            }, os.path.join('saveDir', 'save_voc1.tar'))
     
     def load(self, filename):
         checkpoint = torch.load(filename)
@@ -85,20 +58,19 @@ class Voc:
 class Encoder(nn.Module):
 
     def __init__(self, 
-            emb_size, 
             hidden_size, 
             vocab_size,
             id_to_vec,
+            embedding,
             p_dropout): 
     
             super(Encoder, self).__init__()
-            self.emb_size = emb_size
             self.hidden_size = hidden_size
             self.vocab_size = vocab_size
             self.id_to_vec = id_to_vec
             self.p_dropout = p_dropout
-            self.embedding = nn.Embedding(self.vocab_size, self.emb_size)
-            self.lstm = nn.LSTM(self.emb_size, self.hidden_size)
+            self.embedding = embedding
+            self.lstm = nn.LSTM(self.hidden_size, self.hidden_size)
             self.dropout_layer = nn.Dropout(self.p_dropout) 
 
             self.init_weights()
@@ -108,7 +80,7 @@ class Encoder(nn.Module):
         self.lstm.weight_ih_l0.requires_grad = True
         self.lstm.weight_hh_l0.requires_grad = True
         
-        embedding_weights = torch.FloatTensor(self.vocab_size, self.emb_size)
+        embedding_weights = torch.FloatTensor(self.vocab_size, self.hidden_size)
         embedding_weights = embedding_weights.to(device)
         for id, vec in self.id_to_vec.items():
             embedding_weights[id] = vec
@@ -122,14 +94,6 @@ class Encoder(nn.Module):
         
         _, (last_hidden, _) = self.lstm(packed, hidden)
         last_hidden = self.dropout_layer(last_hidden[-1])
-        return last_hidden
-    
-    def forward(self, inputs):
-        embeddings = self.embedding(inputs)
-        embeddings = embeddings.to(device)
-        _, (last_hidden, _) = self.lstm(embeddings)
-        last_hidden = self.dropout_layer(last_hidden[-1])
-
         return last_hidden
 
     
@@ -145,81 +109,83 @@ class DualEncoder(nn.Module):
         self.M = nn.Parameter(M, requires_grad = True)
 
     def forward(self, input_seq, response_seq, input_lengths, response_lengths):
-        context_last_hidden = self.encoder(input_seq, input_lengths) #dimensions: (batch_size x hidden_size)
+        context_last_hidden = self.encoder(input_seq, input_lengths) 
         context_last_hidden = context_last_hidden.to(device)
-        response_last_hidden = self.encoder(response_seq, response_lengths) #dimensions: (batch_size x hidden_size)
+        response_last_hidden = self.encoder(response_seq, response_lengths) 
         response_last_hidden = response_last_hidden.to(device)
         
-        context = context_last_hidden.mm(self.M) #dimensions: (batch_size x hidden_size)
-        context = context.view(-1, 1, self.hidden_size) #dimensions: (batch_size x 1 x hidden_size)
+        context = context_last_hidden.mm(self.M) 
+        context = context.view(-1, 1, self.hidden_size)
         context = context.to(device)
         
-        response = response_last_hidden.view(-1, self.hidden_size, 1) #dimensions: (batch_size x hidden_size x 1)
+        response = response_last_hidden.view(-1, self.hidden_size, 1)
         response = response.to(device)
         
-        score = torch.bmm(context, response).view(-1, 1) #dimensions: (batch_size x 1 x 1) and lastly --> (batch_size x 1)
+        score = torch.bmm(context, response).view(-1, 1)
         score = score.to(device)
 
         return score
     
-    def forward(self, context_tensor, response_tensor):
-        
-        context_last_hidden = self.encoder(context_tensor) #dimensions: (batch_size x hidden_size)
-        context_last_hidden = context_last_hidden.to(device)
-        
-        response_last_hidden = self.encoder(response_tensor) #dimensions: (batch_size x hidden_size)
-        context = context_last_hidden.mm(self.M) #dimensions: (batch_size x hidden_size)
-        context = context.view(-1, 1, self.hidden_size) #dimensions: (batch_size x 1 x hidden_size)
-        context = context.to(device)
-        
-        response = response_last_hidden.view(-1, self.hidden_size, 1) #dimensions: (batch_size x hidden_size x 1)
-        response = response.to(device)
-        
-        score = torch.bmm(context, response).view(-1, 1) #dimensions: (batch_size x 1 x 1) and lastly --> (batch_size x 1)
-        score = score.to(device)
-
-        return score
-    
-def creating_model(emb_dim, hidden_size, vocab_size, id_to_vec, p_dropout):
+def creating_model(hidden_size, vocab_size, id_to_vec, embedding, p_dropout):
 
     encoder = Encoder(
-            emb_size = emb_dim,
             hidden_size = hidden_size,
             vocab_size = vocab_size,
             id_to_vec = id_to_vec,
+            embedding = embedding,
             p_dropout = p_dropout)
 
     dual_encoder = DualEncoder(encoder)
     
     return encoder, dual_encoder
 
-def get_response_sample(context, num_responses):
+def get_response_sample(context, num_responses=1):
     
-    fileName = 'Tutorials/dataset/models/save_voc.tar'
+    fileName = 'SaveDir/save_voc1.tar'
+    save_file = 'SaveDir/retrieval_model_Val.tar'
     voc = Voc()
     voc.load(fileName)
     word_to_id = voc.word2id
-    context_ids = load_id(context, word_to_id)
+    context_ids = [load_id(context, word_to_id)]
     i = 0
     best_response = ''
     best_score = 0
     
     # Set up encoder
-    encoder, dual_encoder = creating_model(emb_dim = 50, hidden_size = 50, vocab_size = len(voc.vocab), id_to_vec = voc.id2vec, p_dropout = 0.85)
+    hidden_size = 50
+    vocab_size = len(voc.vocab)
+    embedding = nn.Embedding(vocab_size, hidden_size)
+    encoder, dual_encoder = creating_model(hidden_size=hidden_size, vocab_size = len(voc.vocab), id_to_vec = voc.id2vec, embedding=embedding, p_dropout = 0.85)
+    #encoder, dual_encoder = creating_model(emb_dim = 50, hidden_size = 50, vocab_size = len(voc.vocab), id_to_vec = voc.id2vec, p_dropout = 0.85)
     encoder = encoder.to(device)
     dual_encoder = dual_encoder.to(device)
-    dual_encoder.load_state_dict(torch.load('Tutorials/dataset/models/retrieval_encoder_dict.pt'))
+    checkpoint = torch.load(save_file)
+    encoder_sd = checkpoint['en']
+    dual_encoder.load_state_dict(encoder_sd)
     dual_encoder.eval();
     
-    responses = random.sample(voc.sentences, num_responses)
+    if num_responses == 1:
+        responses = voc.sentences
+    else:
+        responses = random.sample(voc.sentences, num_responses)
+        
     for response in responses:
         if not isinstance(response[0], str):
             continue
-        response_ids = load_id(response[0], word_to_id)
-        contextt = autograd.Variable(torch.LongTensor(context_ids).view(-1,1))
-        responset = autograd.Variable(torch.LongTensor(response_ids).view(-1, 1))
-        contextt, responset = contextt.to(device), responset.to(device)
-        score = torch.sigmoid(dual_encoder(contextt, responset))
+        response_ids = [load_id(response[0], word_to_id)]
+        lengths = torch.tensor([len(indexes) for indexes in context_ids])
+        lengths1 = torch.tensor([len(indexes) for indexes in response_ids])
+        context_b = torch.LongTensor(context_ids).transpose(0, 1)
+        response_b = torch.LongTensor(response_ids).transpose(0, 1)
+        
+        context_b = context_b.to(device)
+        response_b = response_b.to(device)
+        
+        lengths = lengths.to("cpu")
+        lengths1 = lengths1.to("cpu")
+        if lengths == 0 or lengths1 == 0:
+            continue
+        score = dual_encoder(context_b, response_b, lengths, lengths1)
         if score > best_score:
             
             best_score = score
